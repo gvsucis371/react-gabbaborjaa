4edb0bc9e49df8d70d75c79ecd81266499700734
gabbaborjaa <gab.borja26@gmail.com>
2025-03-18 22:11:24 -0400

    react app created

---
+++ marvel_heroes/src/App.js
@@ -0,0 +1,11 @@
+import Marvel from './marvel';
+import './App.css';
+
+function App() {
+ return (
+  <div> 
+    <Marvel />
+  </div>
+ ) 
+}
+export default App;
---
+++ marvel_heroes/src/marvel.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Marvel() {
+  return (
+    <div>
+      <h1>Welcome to Marvel Heroes</h1>
+      <p>This is a simple React app to showcase Marvel heroes.</p>
+    </div>
+  );
+}
+
+export default Marvel;
